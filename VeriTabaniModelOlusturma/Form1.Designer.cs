f9af44668650b2796875787f238906f1262e24d0
MARIF <MARIF@MONSTER>
2022-12-19 11:27:23 +0300

    Add project files.

---
+++ VeriTabaniModelOlusturma/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace VeriTabaniModelOlusturma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbVerTabList = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtModelOlusum = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(81, 12);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(356, 20);
+            this.txtServer.TabIndex = 0;
+            this.txtServer.Text = "MONSTER";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(362, 94);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 41);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Bağlantı Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cmbVerTabList);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtSifre);
+            this.panel1.Controls.Add(this.txtUser);
+            this.panel1.Controls.Add(this.txtServer);
+            this.panel1.Controls.Add(this.btnTest);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1002, 202);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(81, 40);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(356, 20);
+            this.txtUser.TabIndex = 0;
+            this.txtUser.Text = "sa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sql User";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(81, 68);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(356, 20);
+            this.txtSifre.TabIndex = 0;
+            this.txtSifre.Text = "12345";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sql Şifre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Veri Tabanı";
+            // 
+            // cmbVerTabList
+            // 
+            this.cmbVerTabList.DisplayMember = "name";
+            this.cmbVerTabList.FormattingEnabled = true;
+            this.cmbVerTabList.Location = new System.Drawing.Point(81, 159);
+            this.cmbVerTabList.Name = "cmbVerTabList";
+            this.cmbVerTabList.Size = new System.Drawing.Size(356, 21);
+            this.cmbVerTabList.TabIndex = 3;
+            this.cmbVerTabList.ValueMember = "name";
+            this.cmbVerTabList.SelectedValueChanged += new System.EventHandler(this.cmbVerTabList_SelectedValueChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.DisplayMember = "TABLE_NAME";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 208);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 407);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.ValueMember = "TABLE_NAME";
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // txtModelOlusum
+            // 
+            this.txtModelOlusum.Location = new System.Drawing.Point(269, 208);
+            this.txtModelOlusum.Name = "txtModelOlusum";
+            this.txtModelOlusum.Size = new System.Drawing.Size(721, 407);
+            this.txtModelOlusum.TabIndex = 4;
+            this.txtModelOlusum.Text = "";
+            this.txtModelOlusum.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 622);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(912, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(931, 622);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Kayıt Yeri";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 660);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 58);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 730);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtModelOlusum);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbVerTabList;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox txtModelOlusum;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VeriTabaniModelOlusturma/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VeriTabaniModelOlusturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        SqlConnection Bag;
+        SqlCommand cmd = new SqlCommand();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VeriTabaniListesi();
+        }
+        void VeriTabaniListesi()
+        {
+            Bag = BagOlustur();
+            if (BaglantiTest(Bag))
+            {
+                if (Bag.State == ConnectionState.Open)
+                {
+                    Bag.Close();
+                }
+                Bag.Open();
+                cmd.Connection = Bag;
+                cmd.CommandText = "SELECT * FROM sys.databases ";
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                cmbVerTabList.DataSource = dt;
+
+            }
+
+        }
+
+        bool BaglantiTest(SqlConnection bag)
+        {
+            try
+            {
+                bag.Open();
+                bag.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        SqlConnection BagOlustur()
+        {
+
+            builder.DataSource = txtServer.Text;
+            builder.Password = txtSifre.Text;
+            builder.UserID = txtUser.Text;
+            return new SqlConnection(builder.ConnectionString);
+
+        }
+
+        private void cmbVerTabList_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (Bag.State == ConnectionState.Closed)
+            {
+                Bag.Open();
+            }
+            cmd.CommandText = $@"SELECT * FROM {cmbVerTabList.SelectedValue}.INFORMATION_SCHEMA.TABLES ORDER BY TABLE_NAME";
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            listBox1.DataSource = dt;
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (Bag.State == ConnectionState.Closed)
+            {
+                Bag.Open();
+            }
+            cmd.CommandText = $@"SELECT * FROM {cmbVerTabList.SelectedValue}.INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='{listBox1.SelectedValue}' ";
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            string Model = "";
+            StringBuilder Models = new StringBuilder();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                //public int BANHARREFNO { get; set; }
+                string turu="";
+                switch (dt.Rows[i]["DATA_TYPE"].ToString())
+                {
+                    case "datetime":
+                        turu = "DateTime ";
+                        break;
+                    case "varchar":
+                        turu = "string ";
+                        break;
+                    case "int":
+                        turu = "int ";
+                        break;
+                    case "numeric":
+                        turu = "decimal ";
+                        break;
+                   
+                }
+             
+
+        Model = $@"public {(turu)}"+ dt.Rows[i]["COLUMN_NAME"].ToString() + " { get; set; }";
+                Models.AppendLine(Model);
+                Model = "";
+            }
+            txtModelOlusum.Text = Models.ToString();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fileDialog = new FolderBrowserDialog();
+            
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text=fileDialog.SelectedPath;
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder build = new StringBuilder();
+            build.Append("using System;\r\nusing System.ComponentModel.DataAnnotations;\r\n\r\nnamespace EtaWeb.Models.Banka\r\n{\r\n    public class "+$"{listBox1.SelectedValue}"+ "\r\n    {\r\n");
+            build.Append(txtModelOlusum.Text);
+            build.Append("\r\n    }\r\n}");
+            StreamWriter stream = new StreamWriter(textBox1.Text+$@"\{listBox1.SelectedValue}.cs");            
+            stream.Write(build.ToString());
+           await stream.FlushAsync();
+            stream.Close();
+        }
+    }
+}
