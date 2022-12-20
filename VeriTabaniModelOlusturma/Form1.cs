ec876c1b3f2ec496b0617a2f04f88a53a5f9cc65
oxside <79248889+oxside@users.noreply.github.com>
2022-12-20 08:49:36 +0300

    Update Form1.cs

--- VeriTabaniModelOlusturma/Form1.cs
+++ VeriTabaniModelOlusturma/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,10 +6,12 @@ using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.ListBox;
 
 namespace VeriTabaniModelOlusturma
 {
@@ -89,41 +91,49 @@ namespace VeriTabaniModelOlusturma
 
         }
 
-        private void listBox1_DoubleClick(object sender, EventArgs e)
+            StringBuilder Models = new StringBuilder();
+        void ModelOlustur(string _modelAdi)
         {
+
             if (Bag.State == ConnectionState.Closed)
             {
                 Bag.Open();
             }
-            cmd.CommandText = $@"SELECT * FROM {cmbVerTabList.SelectedValue}.INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='{listBox1.SelectedValue}' ";
+            cmd.CommandText = $@"SELECT * FROM {cmbVerTabList.SelectedValue}.INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='{_modelAdi}' ";
             DataTable dt = new DataTable();
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             da.Fill(dt);
             string Model = "";
-            StringBuilder Models = new StringBuilder();
             for (int i = 0; i < dt.Rows.Count; i++)
             {
                 //public int BANHARREFNO { get; set; }
-                string turu="";
+                string turu = "";
                 switch (dt.Rows[i]["DATA_TYPE"].ToString())
                 {
                     case "datetime":
                         turu = "DateTime ";
                         break;
                     case "varchar":
-                        turu = "string ";
+                        turu = "string";
                         break;
                     case "int":
-                        turu = "int ";
+                        turu = "int";
                         break;
                     case "numeric":
-                        turu = "decimal ";
+                        turu = "decimal";
                         break;
-                   
-                }
-             
 
-        Model = $@"public {(turu)}"+ dt.Rows[i]["COLUMN_NAME"].ToString() + " { get; set; }";
+                }
+
+                if (turu == "string")
+                {
+                    Models.AppendLine($@"[StringLength({dt.Rows[i]["CHARACTER_MAXIMUM_LENGTH"]})]");
+                }
+                else if (turu == "decimal")
+                {
+                    Models.AppendLine($@"[Column(TypeName = ""decimal({dt.Rows[i]["NUMERIC_PRECISION"]},{dt.Rows[i]["NUMERIC_SCALE"]})"")]");
+                }
+                Model = $@"public {(turu)} " + dt.Rows[i]["COLUMN_NAME"].ToString() + " { get; set; }";
                 Models.AppendLine(Model);
                 Model = "";
             }
@@ -131,26 +141,120 @@ namespace VeriTabaniModelOlusturma
 
         }
 
+
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            ModelOlustur(listBox1.SelectedValue.ToString());
+        }
+        string ProjeAdi;
+        string ProjeYolu;
+        string ModelNamespace = "";
         private void button1_Click_1(object sender, EventArgs e)
         {
             FolderBrowserDialog fileDialog = new FolderBrowserDialog();
-            fileDialog.SelectedPath = textBox1.Text;
+            fileDialog.SelectedPath =ProjeYolu;
             if (fileDialog.ShowDialog() == DialogResult.OK)
             {
-                textBox1.Text=fileDialog.SelectedPath;
+
+                textBox1.Text = fileDialog.SelectedPath;
+                ProjeYolu = fileDialog.SelectedPath;
+                string[] Klasorler = textBox1.Text.Split(new string[] { ProjeAdi }, StringSplitOptions.None);
+                string[] Yol = Klasorler[2].Split('\\');
+                    StringBuilder yolOlusur = new StringBuilder();
+                for (int i = 0; i < Yol.Length; i++)
+                {
+                    string yolKademe =  Yol[i].Trim();
+                    if (i == 0)
+                    {
+                        yolKademe = ProjeAdi;
+                    }
+                    else
+                    {
+
+                    }
+                    yolKademe = yolKademe + (i + 1 != Yol.Length ? "." : "");
+                    yolOlusur.AppendLine(yolKademe);
+                    ModelNamespace=yolOlusur.ToString();
+                }
+
+            }
+        }
+        async Task ModelYazAsync(string _model,string _modelAdi)
+        {
+            StringBuilder build = new StringBuilder();
+            build.Append("using Microsoft.EntityFrameworkCore.Metadata.Internal;\r\nusing System;\r\nusing System.ComponentModel.DataAnnotations;\r\nusing System.ComponentModel.DataAnnotations.Schema;\r\n\r\nnamespace " + $"{ProjeAdi}.Models.Banka\r\n" + "{   public class " + $"{_modelAdi}" + "\r\n    {\r\n");
+            build.Append(_model);
+            build.Append("\r\n    }\r\n}");
+            StreamWriter stream = new StreamWriter(textBox1.Text + $@"\{_modelAdi}.cs");
+            stream.Write(build.ToString());
+            await stream.FlushAsync();
+            stream.Close();
+
+        }
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            Models.Clear();
+            if (string.IsNullOrWhiteSpace(ModelNamespace) || string.IsNullOrWhiteSpace(ProjeYolu))
+            {
+                return;
+            }
+            if (checkBox1.Checked)
+            {
+                foreach (var item in listBox1.SelectedItems)
+                {
+
+                    //MessageBox.Show(listBox1.GetItemText(item).ToString());
+                    listBox1.SelectedItem = item;
+                    ModelOlustur(listBox1.GetItemText(item));
+                    await ModelYazAsync(Models.ToString(), listBox1.GetItemText(item).ToString());
+                }
+
+
+
             }
         }
 
-        private async void button2_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            StringBuilder build = new StringBuilder();
-            build.Append("using System;\r\nusing System.ComponentModel.DataAnnotations;\r\n\r\nnamespace EtaWeb.Models.Banka\r\n{\r\n    public class "+$"{listBox1.SelectedValue}"+ "\r\n    {\r\n");
-            build.Append(txtModelOlusum.Text);
-            build.Append("\r\n    }\r\n}");
-            StreamWriter stream = new StreamWriter(textBox1.Text+$@"\{listBox1.SelectedValue}.cs");            
-            stream.Write(build.ToString());
-           await stream.FlushAsync();
-            stream.Close();
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ProjeYolu = fileDialog.FileName.Replace(fileDialog.SafeFileName,"");
+                FileStream fs = new FileStream(fileDialog.FileName, FileMode.Open);
+
+                StreamReader sr = new StreamReader(fs);
+                StringBuilder Yazi = new StringBuilder();
+                while (!sr.EndOfStream)
+                {
+                    string Okunan = sr.ReadLine();
+                    if (Okunan.StartsWith("namespace "))
+                    {
+                        ProjeAdi = (Okunan.Replace("namespace ", ""));
+                        break;
+                    }
+                }
+                fs.Close();
+                sr.Close();
+                txtprojeAdi.Text = ProjeAdi;
+            }
+
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox1.SelectedItems.Clear();
+            if (checkBox1.Checked)
+            {
+            listBox1.SelectionMode = SelectionMode.MultiExtended;
+            }
+            else
+            {
+                listBox1.SelectionMode = SelectionMode.One;
+
+            }
+
         }
     }
 }
